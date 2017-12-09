6a7863dbe08587ff7c94e9a6302de016588ed3a5
Valter Martinek <martinek@freevision.sk>
2017-12-09 20:44:34 +0100

    Styling.

--- src/components/layout/LayoutContainer.js
+++ src/components/layout/LayoutContainer.js
@@ -11,23 +11,23 @@ const LayoutContainer = ({children}) => (
         <div className="hero-foot">
             <footer className="footer">
                 <div className="container has-text-centered">
-                    <p>
+                    <p className="credits">
                             ©&nbsp;<a href="https://www.freevision.sk" target="new">freevision.sk</a>&nbsp;2017 | 
                             Created by <a href="https://www.freevision.sk" target="new">freevision.sk</a> | 
                             Market&nbsp;data&nbsp;by&nbsp;<a href="https://www.cryptocompare.com/" target="new">cryptocompare.com</a>
                     </p>
                     <p className="version">Version: {BUILD.VERSION}</p>
-                    <p className="donation" onClick={() => modal(
+                    <a className="button is-primary is-inverted is-outlined donation" onClick={() => modal(
                         <div className="message is-info donation-info has-text-centered">
                             <div className="message-body">
                                 <img src="1457PgXio7wV8vQmD1dxpBLNGUaAfDjSUV.png" />
-                                <p>
+                                <p className="donation-address">
                                     <span className="fab fa-bitcoin" />
                                     <span>1457PgXio7wV8vQmD1dxpBLNGUaAfDjSUV</span>
                                 </p>
                             </div>
                         </div>
-                    )}>Donate BTC</p>
+                    )}>Donate BTC</a>
                 </div>
             </footer>     
         </div>
