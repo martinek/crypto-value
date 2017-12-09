eef6a112a90286ed83dc8da7ad6c805e95f25030
Valter Martinek <martinek@freevision.sk>
2017-12-09 21:37:01 +0100

    Add info icon and text.

--- src/components/CardHeader.js
+++ src/components/CardHeader.js
@@ -14,6 +14,24 @@ const CardHeader = ({children, back}) => {
                     </Link>
                     : <span>{title}</span>
                 }
+                <a className="info-icon has-text-primary" onClick={() => modal((
+                    <div className="message is-info donation-info">
+                        <div className="message-body">
+                            <div className="content">
+                                <p><strong>What is this?</strong></p>
+                                <p>crypto-value.info is a website where you can track current value of cryptocurrency you own in a simple and practical way.</p>
+                                <strong>Are my data safe?</strong>
+                                <ul>
+                                    <li>All data about your holdings is stored <strong>IN YOUR BROWSER</strong> using local storage technology.</li>
+                                    <li>Your data <strong>IS NOT</strong> stored on our servers. We <strong>DO NOT</strong> know how much you have, we don’t care.</li>
+                                    <li>Use this website at your own risk.</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                ))}>
+                    <span className="fas fa-info-circle" />
+                </a>
             </p>
             {children}
         </header>
