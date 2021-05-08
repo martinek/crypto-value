d4711345062add8ba0efa9c44e782e288e1b2fd2
Valter Martinek <martinek@freevision.sk>
2021-05-08 12:52:42 +0200

    Replace project with CRA typescript template

--- src/constants/ActionTypes.js
+++
@@ -1,7 +0,0 @@
-const ActionTypes = {
-    USER: {
-        SET_DATA: 'USER_SET_DATA',
-    },
-};
-
-export default ActionTypes;
--- src/reducers/rootReducer.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux';
-
-import userDataReducer from './userDataReducer';
-
-const rootReducer = combineReducers({
-    userData: userDataReducer,
-});
-
-export default rootReducer;
